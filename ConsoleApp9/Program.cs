4375520011841068f962726dbdf2c7a47d3247ca
hrafag <afagnd@code.edu.az>
2022-04-21 19:01:39 +0400

    new task is added

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User ();
+            user.Username = "itShain";
+            user.Password = "itSh123456";
+
+            Admin admin = new Admin();
+            admin.Section = "Section";
+            admin.IsSuperAdmin = true;
+                    
+         }
+        
+        class Admin 
+        {
+            public string Section;
+            public bool IsSuperAdmin;
+        } 
+
+        class User
+        {
+            public string Username;
+            public string Password;
+
+              
+                     
+        }
+        
+   
+    }
+}
